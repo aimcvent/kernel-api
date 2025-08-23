cfa736929aa8eed4f9df776b15f2a7527fb5156a
Neutron-Pro <neutron@neutronstars.fr>
2025-08-23 22:28:03 +0200

    Rename la methode en adapt

--- src/main/java/fr/aimcvent/kernel/api/injector/injection/adapter/ClassAdapter.java
+++ src/main/java/fr/aimcvent/kernel/api/injector/injection/adapter/ClassAdapter.java
@@ -1,5 +1,5 @@
 package fr.aimcvent.kernel.api.injector.injection.adapter;
 
 public interface ClassAdapter<T> extends Adapter<T> {
-    void accept(Class<? extends T> clazz);
+    void adapt(Class<? extends T> clazz);
 }
