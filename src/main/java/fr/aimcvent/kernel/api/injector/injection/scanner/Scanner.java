2eb027f92851c2d5f5b51061c938a92a5f5e4039
Neutron-Pro <neutron@neutronstars.fr>
2025-08-26 14:44:04 +0200

    Modification du scan, prise en compte du module

--- src/main/java/fr/aimcvent/kernel/api/injector/injection/scanner/Scanner.java
+++ src/main/java/fr/aimcvent/kernel/api/injector/injection/scanner/Scanner.java
@@ -1,11 +1,12 @@
 package fr.aimcvent.kernel.api.injector.injection.scanner;
 
 import fr.aimcvent.kernel.api.injector.injection.scanner.filter.Filters;
+import fr.aimcvent.kernel.api.module.Module;
 
 public interface Scanner {
     Filters filters();
 
-    void scan(ClassLoader classLoader, String packageRoot);
+    void scan(Module module);
 
     ScannerResult find(String... packages);
 
